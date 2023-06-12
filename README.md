1249f10b264d300869d9ad5d9f2bbc3c64b780e3
veldakarimi <veldakarimi@gmail.com>
2023-06-12 20:24:53 +0300

    update with presentation and spec link

---
+++ README.md
@@ -0,0 +1,8 @@
+# OSCAFEST23: Building Powerful APIs Using Django, Django Rest Framework, Open API Spec
+
+This is a repo with the code sample I used for my OSCAFest Talk.
+
+
+The slides can be found here: [Canva](https://www.canva.com/design/DAFlGpPhGKw/2LUDc3uwrKKrm8-KYrv4XQ/edit?utm_content=DAFlGpPhGKw&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton)
+
+The API spec can be found here; [OpenAPI](https://app.swaggerhub.com/apis/veldakarimi/oscafest23/1.0.0)
--- merchandises/urls.py
+++ merchandises/urls.py
@@ -1,27 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView
-# from access import views
-app_name='merchandises'
-
-# urlpatterns=[
-# path('', TemplateView.as_view(template_name='home.html'), name='home'),
-# path('merchandises/', name='merchandise'),
-
-# # path('hello/', views.HelloView.as_view(), name='hello'),
-
-# ]
-
-
 from .views import MerchandiseListView, MerchandiseDetailView
 
+app_name='merchandises'
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    # path('merchandise/', MerchandiseListAPIView.as_view(), name='merchandise-list'),
-    # path('merchandise/<int:pk>/', MerchandiseDetailAPIView.as_view(), name='merchandise-detail'),
     path('merchandise/', MerchandiseListView.as_view(), name='merchandise-list'),
     path('merchandise/<uuid:pk>/', MerchandiseDetailView.as_view(), name='merchandise-detail'),
-
-
 ]
-
